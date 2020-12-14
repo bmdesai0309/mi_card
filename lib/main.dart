4fd8f817e767497c3545432f7c245bdc0c0f99e7
bmdesai0309 <bmdesai1685@gmail.com>
2020-12-14 15:16:53 +0530

    Add Container and edit some Text and use margin and padding..

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,17 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.blue,
-        body: Container(),
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Container(
+            height: 100.0,
+            width: 100.0,
+            margin: EdgeInsets.only(left: 10.0, top: 10.0),
+            padding: EdgeInsets.only(left: 10.0, top: 10.0),
+            color: Colors.white,
+            child: Text('Hello'),
+          ),
+        ),
       ),
     );
   }
