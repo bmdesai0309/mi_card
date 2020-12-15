ff0dc9da86368a94183b2ca86b7a24736cfe7292
bmdesai0309 <bmdesai1685@gmail.com>
2020-12-15 18:21:23 +0530

    Complete the app

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
             child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             CircleAvatar(
               radius: 50.0,
@@ -21,23 +22,55 @@ class MyApp extends StatelessWidget {
             Text(
               'Balmukund Desai',
               style: TextStyle(
-                fontSize: 40.0,
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
-                fontFamily: 'Pacifico'
-              ),
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Pacifico'),
             ),
             Text(
               'FLUTTER DEVELOPER',
               style: TextStyle(
-                letterSpacing: 2.5,
-                fontSize: 20.0,
+                  letterSpacing: 2.5,
+                  fontSize: 20.0,
+                  color: Colors.teal.shade100,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'SourceSansPro'),
+            ),
+            SizedBox(
+              width: 150.0,
+              height: 20.0,
+              child: Divider(
                 color: Colors.teal.shade100,
-                fontWeight: FontWeight.bold,
-                fontFamily: 'SourceSansPro'
-
               ),
-            )
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: ListTile(
+                leading: Icon(Icons.phone, color: Colors.teal),
+                title: Text(
+                  '+91 9624911523',
+                  style: TextStyle(
+                      color: Colors.teal.shade900,
+                      fontFamily: 'SourceSansPro',
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.w600),
+                ),
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: ListTile(
+                leading: Icon(Icons.email, color: Colors.teal),
+                title: Text(
+                  'bmdesai1685@gmail.com',
+                  style: TextStyle(
+                      color: Colors.teal.shade900,
+                      fontFamily: 'SourceSansPro',
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.w600),
+                ),
+              ),
+            ),
           ],
         )),
       ),
